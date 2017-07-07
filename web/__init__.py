ffc27e53b5dc5c2160c10ff0ef0cd29e5c954943
gnilly <gnillyboy@gmail.com>
2017-07-08 01:41:32 +0300

    cleanup

--- web/__init__.py
+++ web/__init__.py
@@ -3,20 +3,13 @@ import logging
 import logging.config
 
 from aiohttp import web
-import aiohttp
 import aiohttp_jinja2
 import jinja2
 
-import requests
-
 import pandas as pd
-import numpy as np
-import json,os
+import json
+import os
 import datetime as dt
-
-from web.router import configure_handlers, routes
-from web.utils.assets import AssetManager
-from web.utils.settings import get_config
 
 import sqlalchemy
 
@@ -27,66 +20,37 @@ log = logging.getLogger(__name__)
 with open('config.json') as json_data:
     config = json.load(json_data)
 
+
 def connect_db(url):
     con = sqlalchemy.create_engine(url, client_encoding='utf8')
     meta = sqlalchemy.MetaData(bind=con, reflect=True)
     return con, meta
 
-# obtain token, subscribe to topics
-r = requests.get('https://api.vk.com/method/streaming.getServerUrl',
-                 params={'access_token': '9222b7889222b7889222b788fa927fa290992229222b788cb78779aea9215c6c94abac3'})
-[api_host, access_key] = r.json()['response']['endpoint'], r.json()['response']['key']
-
-r = requests.get('https://{}/rules?key={}'.format(api_host, access_key))
-if r.json()['rules'] is not None:
-    for x in r.json()['rules']:
-        r = requests.delete('https://{}/rules?key={}'.format(api_host, access_key), json={'tag': x['tag']})
-
-rule_names = []
-for i in range(len(config)):
-    payload = {
-        'rule': {'value': config[i]['keywords'], 'tag': config[i]['topic']}
-    }
-    rule_names.append(config[i]['topic'])
-    r = requests.post('https://{}/rules?key={}'.format(api_host, access_key), json=payload)
-
-
-wss_url = 'wss://{}/stream?key={}'.format(api_host, access_key)
-
 # prepare emoji sentiment source data + formula from the paper
-
 emoji = pd.read_csv('emoji.csv')
 emoji['sentiment_score'] = (emoji['Positive'] + 1) / \
                            (emoji['Occurrences'] + 3) - (emoji['Negative'] + 1) / (emoji['Occurrences'] + 3)
 
 emoji_dict = set(list(emoji['Emoji']))
 
-# init stats data structure
-
 con, meta = connect_db(os.environ["DATABASE_URL"])
 
+rule_names = [c['topic'] for c in config]
+
+
 def round_time(t):
-    # t - dt.timedelta(minutes=t.minute,seconds=t.second,microseconds=t.microsecond)
     return t - dt.timedelta(seconds=t.second, microseconds=t.microsecond)
 
-
 async def main_page_handler(request):
-    overall_sent, top_emoji = pd.read_sql('overall_sent',con), pd.read_sql('top_emoji',con)
+    overall_sent, top_emoji = pd.read_sql('overall_sent', con), pd.read_sql('top_emoji', con)
 
     overall_sent.set_index('tag', inplace=True)
     top_emoji.drop('index', 1, inplace=True)
 
-    #print(overall_sent)
-    #print(top_emoji)
-
-    chart_data = pd.read_sql_table('chart_data',con)
+    chart_data = pd.read_sql_table('chart_data', con)
     chart_data.set_index(['tag', 'time-slot'], inplace=True)
 
     tag = request.query['tag'] if 'tag' in request.query else rule_names[0]
-
-
-    #print(tag)
-    #print(rule_names)
 
     def get_top_emoji_by_tag(tag):
         if len(top_emoji) == 0:
@@ -99,15 +63,15 @@ async def main_page_handler(request):
         return ''.join(list(top_emoji[top_emoji['tag'] == tag]['emoji']))
 
     def get_chart(tag):
-        if len(chart_data)==0:
-            return {'x':[],'y':[]}
+        if len(chart_data) == 0:
+            return {'x': [], 'y': []}
         chart = chart_data.reset_index()[chart_data.reset_index()['tag'] == tag].drop('tag', 1)
         return {'x': list(chart['time-slot'].astype(str)), 'y': list(chart['sent'])}
 
     def get_sent(tag):
-        if len(top_emoji)==0:
+        if len(top_emoji) == 0:
             return 0
-        return overall_sent.ix[tag,0] if tag in overall_sent.index else 0
+        return overall_sent.ix[tag, 0] if tag in overall_sent.index else 0
 
     emoji_data = {'top': get_top_emoji_by_tag(tag),
                   'emoji_str': get_top_emoji_str(tag),
@@ -152,7 +116,7 @@ def build_app(settings_path, loop=None):
     # templates
 
     template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-    env = aiohttp_jinja2.setup(
+    aiohttp_jinja2.setup(
         application, loader=jinja2.FileSystemLoader(template_dir)
     )
 
--- web/handlers.py
+++
@@ -1,9 +0,0 @@
-import aiohttp_jinja2
-import logging
-
-log = logging.getLogger(__name__)
-
-
-@aiohttp_jinja2.template('index.jinja2')
-async def index(request):
-    return {}
